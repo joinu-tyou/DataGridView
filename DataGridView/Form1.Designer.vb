e7df895dba81d30b03316cc902ace26cf43c6805
Y.Zhang <joinus_tyou@brainassist.com>
2020-01-10 21:29:08 +0900

    InitGridNoDataSource

--- DataGridView/Form1.Designer.vb
+++ DataGridView/Form1.Designer.vb
@@ -22,17 +22,161 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.fg12 = New System.Windows.Forms.DataGridView()
+        Me.fg11 = New System.Windows.Forms.DataGridView()
+        Me.fg10 = New System.Windows.Forms.DataGridView()
+        Me.fg9 = New System.Windows.Forms.DataGridView()
+        Me.fg8 = New System.Windows.Forms.DataGridView()
+        Me.fg7 = New System.Windows.Forms.DataGridView()
+        Me.fg6 = New System.Windows.Forms.DataGridView()
+        Me.fg5 = New System.Windows.Forms.DataGridView()
+        Me.fg4 = New System.Windows.Forms.DataGridView()
+        Me.fg3 = New System.Windows.Forms.DataGridView()
+        Me.fg2 = New System.Windows.Forms.DataGridView()
+        Me.fg1 = New System.Windows.Forms.DataGridView()
+        CType(Me.fg12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.fg11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.fg10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.fg9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.fg8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.fg7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.fg6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.fg5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.fg4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.fg3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.fg2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.fg1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'fg12
+        '
+        Me.fg12.Location = New System.Drawing.Point(812, 469)
+        Me.fg12.Name = "fg12"
+        Me.fg12.Size = New System.Drawing.Size(232, 160)
+        Me.fg12.TabIndex = 24
+        '
+        'fg11
+        '
+        Me.fg11.Location = New System.Drawing.Point(562, 469)
+        Me.fg11.Name = "fg11"
+        Me.fg11.Size = New System.Drawing.Size(232, 160)
+        Me.fg11.TabIndex = 23
+        '
+        'fg10
+        '
+        Me.fg10.Location = New System.Drawing.Point(312, 469)
+        Me.fg10.Name = "fg10"
+        Me.fg10.Size = New System.Drawing.Size(232, 160)
+        Me.fg10.TabIndex = 22
+        '
+        'fg9
+        '
+        Me.fg9.Location = New System.Drawing.Point(62, 469)
+        Me.fg9.Name = "fg9"
+        Me.fg9.Size = New System.Drawing.Size(232, 160)
+        Me.fg9.TabIndex = 21
+        '
+        'fg8
+        '
+        Me.fg8.Location = New System.Drawing.Point(812, 289)
+        Me.fg8.Name = "fg8"
+        Me.fg8.Size = New System.Drawing.Size(232, 160)
+        Me.fg8.TabIndex = 20
+        '
+        'fg7
+        '
+        Me.fg7.Location = New System.Drawing.Point(562, 289)
+        Me.fg7.Name = "fg7"
+        Me.fg7.Size = New System.Drawing.Size(232, 160)
+        Me.fg7.TabIndex = 19
+        '
+        'fg6
+        '
+        Me.fg6.Location = New System.Drawing.Point(312, 289)
+        Me.fg6.Name = "fg6"
+        Me.fg6.Size = New System.Drawing.Size(232, 160)
+        Me.fg6.TabIndex = 18
+        '
+        'fg5
+        '
+        Me.fg5.Location = New System.Drawing.Point(62, 289)
+        Me.fg5.Name = "fg5"
+        Me.fg5.Size = New System.Drawing.Size(232, 160)
+        Me.fg5.TabIndex = 17
+        '
+        'fg4
+        '
+        Me.fg4.Location = New System.Drawing.Point(812, 109)
+        Me.fg4.Name = "fg4"
+        Me.fg4.Size = New System.Drawing.Size(232, 160)
+        Me.fg4.TabIndex = 16
+        '
+        'fg3
+        '
+        Me.fg3.Location = New System.Drawing.Point(562, 109)
+        Me.fg3.Name = "fg3"
+        Me.fg3.Size = New System.Drawing.Size(232, 160)
+        Me.fg3.TabIndex = 15
+        '
+        'fg2
+        '
+        Me.fg2.Location = New System.Drawing.Point(312, 109)
+        Me.fg2.Name = "fg2"
+        Me.fg2.Size = New System.Drawing.Size(232, 160)
+        Me.fg2.TabIndex = 14
+        '
+        'fg1
+        '
+        Me.fg1.Location = New System.Drawing.Point(62, 109)
+        Me.fg1.Name = "fg1"
+        Me.fg1.Size = New System.Drawing.Size(232, 160)
+        Me.fg1.TabIndex = 13
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1143, 704)
+        Me.Controls.Add(Me.fg12)
+        Me.Controls.Add(Me.fg11)
+        Me.Controls.Add(Me.fg10)
+        Me.Controls.Add(Me.fg9)
+        Me.Controls.Add(Me.fg8)
+        Me.Controls.Add(Me.fg7)
+        Me.Controls.Add(Me.fg6)
+        Me.Controls.Add(Me.fg5)
+        Me.Controls.Add(Me.fg4)
+        Me.Controls.Add(Me.fg3)
+        Me.Controls.Add(Me.fg2)
+        Me.Controls.Add(Me.fg1)
         Me.Name = "Form1"
         Me.Text = "DataGridView"
+        CType(Me.fg12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.fg11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.fg10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.fg9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.fg8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.fg7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.fg6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.fg5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.fg4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.fg3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.fg2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.fg1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents fg12 As Windows.Forms.DataGridView
+    Friend WithEvents fg11 As Windows.Forms.DataGridView
+    Friend WithEvents fg10 As Windows.Forms.DataGridView
+    Friend WithEvents fg9 As Windows.Forms.DataGridView
+    Friend WithEvents fg8 As Windows.Forms.DataGridView
+    Friend WithEvents fg7 As Windows.Forms.DataGridView
+    Friend WithEvents fg6 As Windows.Forms.DataGridView
+    Friend WithEvents fg5 As Windows.Forms.DataGridView
+    Friend WithEvents fg4 As Windows.Forms.DataGridView
+    Friend WithEvents fg3 As Windows.Forms.DataGridView
+    Friend WithEvents fg2 As Windows.Forms.DataGridView
+    Friend WithEvents fg1 As Windows.Forms.DataGridView
 End Class
--- DataGridView/Form1.vb
+++ DataGridView/Form1.vb
@@ -1,3 +1,98 @@
-﻿Public Class Form1
+﻿Option Explicit On
+
+Imports System.Reflection.MethodBase
+Imports System.Drawing.ColorTranslator
+Imports System.Text.RegularExpressions
+Imports System.Windows.Forms
+
+Public Class Form1
+
+
+    'グリッドタイトルの色
+    Public Const G_GRID_TITLE_COLOR As String = "#ffdddddd"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        InitGridNoDataSource(fg1)
+
+    End Sub
+
+
+    Private Sub InitGridNoDataSource(ByRef fg As System.Windows.Forms.DataGridView)
+
+        On Error GoTo ERR_HANDLE
+
+        fg.SuspendLayout()
+
+        '初期化
+        fg.AllowUserToOrderColumns = False
+        fg.AllowUserToDeleteRows = False
+        fg.AllowUserToAddRows = False
+        fg.AllowUserToResizeColumns = False
+        fg.AllowUserToResizeRows = False
+        fg.RowHeadersVisible = False
+        fg.ColumnHeadersVisible = False
+        fg.RowTemplate.Height = 285 / 10
+
+
+
+        fg.RowCount = 4
+        fg.ColumnCount = 3
+
+
+        '第一列の設定
+        fg.Columns(0).Width = 50
+        fg.Columns(0).ReadOnly = True
+        fg.Columns(0).DefaultCellStyle.BackColor = FromHtml(G_GRID_TITLE_COLOR)
+
+        '第二列の設定
+        fg.Columns(1).Width = 80
+
+        '第三列の設定
+        fg.Columns(2).Width = 80
+
+        '第一行の設定
+        fg.Rows(0).ReadOnly = True
+        fg.Rows(0).DefaultCellStyle.BackColor = FromHtml(G_GRID_TITLE_COLOR)
+
+        '第一行第一列の設定
+        fg.Rows(0).Cells(0).ReadOnly = False
+        fg.Rows(0).Cells(0).Style.BackColor = Color.White
+
+
+
+
+        fg.Rows(0).Cells(0).Value = "名前"
+
+
+        fg.Rows(0).Cells(1).Value = "点数"
+        fg.Rows(0).Cells(2).Value = "偏差値"
+
+        fg.Rows(1).Cells(0).Value = "英語"
+        fg.Rows(1).Cells(1).Value = ""
+        fg.Rows(1).Cells(2).Value = ""
+
+        fg.Rows(2).Cells(0).Value = "物理"
+        fg.Rows(2).Cells(1).Value = ""
+        fg.Rows(2).Cells(2).Value = ""
+
+        fg.Rows(3).Cells(0).Value = "化学"
+        fg.Rows(3).Cells(1).Value = ""
+        fg.Rows(3).Cells(2).Value = ""
+
+        'For i As Integer = 0 To 3
+        '    fg.Rows(i).Height = 285 / 15 '.set_RowHeight(i, 285)
+        'Next
+
+        fg.Rows(0).Cells(0).Style.BackColor = Color.White
+
+        fg.ResumeLayout()
+        Exit Sub
+ERR_HANDLE:
+        fg.ResumeLayout()
+        MsgBox(GetCurrentMethod.Name & vbCrLf & Err.Description)
+
+
+    End Sub
 
 End Class
